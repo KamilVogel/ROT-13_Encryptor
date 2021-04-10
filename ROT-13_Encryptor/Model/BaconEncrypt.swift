a22654c661248cc7a53cb7d94f02d223a3d80104
Kamil Vogel <kamil7702@gmail.com>
2021-04-10 02:47:00 +0200

    improvments for bacons

--- ROT-13_Encryptor/Model/BaconEncrypt.swift
+++ ROT-13_Encryptor/Model/BaconEncrypt.swift
@@ -10,25 +10,77 @@ import Foundation
 struct BaconEncrypt {
     
     var output: String = ""
-    let dictionaryOfCypher = ["A": "aaaaa", "B": "aaaab", "C": "aaaba", "D": "aaabb", "E": "aabaa", "F": "aabab", "G": "aabba", "H": "aabbb", "I": "abaaa", "J": "abaab", "K": "ababa", "L": "ababb", "M": "ababb", "N": "abbab", "O": "abbba", "P": "abbbb", "Q": "baaaa", "R": "baaab", "S": "baaba", "T": "baabb", "U": "babaa", "V": "babab", "W": "babba", "X": "babbb", "Y": "bbaaa", "Z": "bbaab","aaaaa": "A", "aaaab": "B", "aaaba": "C", "aaabb": "D", "aabaa": "E", "aabab": "F", "aabba": "G", "aabbb": "H", "abaaa": "I", "abaab": "J", "ababa": "K", "ababb": "L", "abbaa": "M", "abbab": "N", "abbba": "O", "abbbb": "P", "baaaa": "Q", "baaab": "R", "baaba": "S", "baabb": "T", "babaa": "U", "babab": "V", "babba": "W", "babbb": "X", "bbaaa": "Y", "bbaab": "Z"]
-
+    
+    let dictionaryOfCypher = [
+        "A": "aaaaa",
+        "B": "aaaab",
+        "C": "aaaba",
+        "D": "aaabb",
+        "E": "aabaa",
+        "F": "aabab",
+        "G": "aabba",
+        "H": "aabbb",
+        "I": "abaaa",
+        "J": "abaab",
+        "K": "ababa",
+        "L": "ababb",
+        "M": "abbaa",
+        "N": "abbab",
+        "O": "abbba",
+        "P": "abbbb",
+        "Q": "baaaa",
+        "R": "baaab",
+        "S": "baaba",
+        "T": "baabb",
+        "U": "babaa",
+        "V": "babab",
+        "W": "babba",
+        "X": "babbb",
+        "Y": "bbaaa",
+        "Z": "bbaab",
+        
+        "aaaaa": "A",
+        "aaaab": "B",
+        "aaaba": "C",
+        "aaabb": "D",
+        "aabaa": "E",
+        "aabab": "F",
+        "aabba": "G",
+        "aabbb": "H",
+        "abaaa": "I",
+        "abaab": "J",
+        "ababa": "K",
+        "ababb": "L",
+        "abbaa": "M",
+        "abbab": "N",
+        "abbba": "O",
+        "abbbb": "P",
+        "baaaa": "Q",
+        "baaab": "R",
+        "baaba": "S",
+        "baabb": "T",
+        "babaa": "U",
+        "babab": "V",
+        "babba": "W",
+        "babbb": "X",
+        "bbaaa": "Y",
+        "bbaab": "Z"
+    ]
+    
     mutating func encryptBacon(textToEncrypt: String, decrypt: Bool) -> String {
     
         output = ""
         let asciiValues = textToEncrypt.uppercased().compactMap { $0 }
         
-        // encryption only
         for var encryption in 0..<asciiValues.count {
             let letter = asciiValues[encryption]
-            let encrypt = dictionaryOfCypher["\(letter)"] ?? " "
-            print(letter)
-            print(encrypt)
-            output += encrypt
+            if let encrypt = dictionaryOfCypher["\(letter)"] {output += encrypt + " "}
             encryption += 5
         }
+        output.removeLast()
         return output
     }
-    // Not Working yet correctly
+    
     mutating func dencryptBacon(textToEncrypt: String, decrypt: Bool) -> String {
     
         output = ""
@@ -36,26 +88,26 @@ struct BaconEncrypt {
         var oneLetter = ""
         let asciiValues = textToEncrypt.lowercased().compactMap { $0 }
         
-        // NOT YET WORKING
         for encryption in 0..<asciiValues.count {
             let letter = asciiValues[encryption]
-            //print(letter)
             divideIntoFiveLetters += 1
-            print("number is \(encryption + 1)")
-            if divideIntoFiveLetters % 5 == 0 {
-                oneLetter += String(letter)
-                let encrypt = dictionaryOfCypher["\(oneLetter)"] ?? " "
-                print(encrypt)
-                //output += encrypt
-                //oneLetter = ""
-                //print("Done")
-            } else {
-                oneLetter += String(letter)
+            switch letter {
+            case "a", "b":
+                if divideIntoFiveLetters % 5 == 0 {
+                    oneLetter += String(letter)
+                    let encrypt = dictionaryOfCypher["\(oneLetter)"]!
+                    output += encrypt
+                    oneLetter = ""
+                } else {
+                    oneLetter += String(letter)
+                }
+            case " ":
+                divideIntoFiveLetters -= 1
+            default:
+                divideIntoFiveLetters -= 1
+                output += String(letter)
             }
-            //output += encrypt // Cannot convert value of type '(UnsafeMutablePointer<Int8>?, Int32) -> Void' to expected argument type 'String'
-
         }
         return output
     }
-    
 }
